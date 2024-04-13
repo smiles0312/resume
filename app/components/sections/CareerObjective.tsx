87ada45038493206edca91cdbed866addf193991
Scott Milewski <scottmilewski@carfax.com>
2024-04-13 18:44:36 -0500

    update career objective

--- app/components/sections/CareerObjective.tsx
+++ app/components/sections/CareerObjective.tsx
@@ -5,17 +5,17 @@ const CareerObjective: FC = () => {
     <div className="section">
       <h3>CAREER OBJECTIVE</h3>
       <p>
-        Senior Front-End Developer with over 10 years of proven leadership with
-        a progressive and diversified technological background. I have a
-        results-driven mentality. I am enthusiastic about learning, teaching,
-        and implementing efficient technologies to make aggressive deadlines
-        consistently achievable. I am passionate about going the extra mile to
-        ensure the customer is unquestionably satisfied with the product or
-        service provided. I am a self-starter with excellent verbal and written
-        communication skills with the ability to interact with business and
-        technical providers. I am able to work on multiple, concurrent projects.
-        I have demonstrated a strong ability to solve problems as well as grasp
-        and adapt new information, technologies and concepts quickly.
+        Senior Front-End Software Engineer with over 10 years of proven
+        leadership with a progressive and diversified technological background.
+        I have a results-driven mentality. I am enthusiastic about learning,
+        teaching, and implementing efficient technologies to make aggressive
+        deadlines consistently achievable. I am passionate about going the extra
+        mile to ensure the customer is unquestionably satisfied with the product
+        or service provided. I am a self-starter with excellent verbal and
+        written communication skills with the ability to interact with business
+        and technical providers. I am able to work on multiple, concurrent
+        projects. I have demonstrated a strong ability to solve problems as well
+        as grasp and adapt new information, technologies and concepts quickly.
       </p>
     </div>
   );
